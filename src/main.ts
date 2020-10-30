6ec951649b7e798a74156b47369ea922d4e8a438
Max Zakharzhevskiy <maxzz@msn.com>
2020-10-29 19:58:27 -0700

    Move bkg ouside of main

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App)
+const app = createApp(App);
+app
     .use(store)
     .use(router)
     .mount("#app");
