0603561e4c65897d5293331ee29b1d6702e8848c
Max Zakharzhevskiy <maxzz@msn.com>
2022-03-01 17:09:54 -0800

    Fix router hosting path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    base: '',
-    plugins: [vue()]
-});
+export default ({command}) => {
+    return defineConfig({
+        base: command === 'build' ? '/test-pm-domain-logins/' : '',
+        plugins: [vue()]
+    });
+};
